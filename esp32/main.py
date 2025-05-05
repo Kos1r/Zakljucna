6d0106298162408853a3ccb81921ae9a125c2a37
Kos1r <128988818+Kos1r@users.noreply.github.com>
2025-05-05 21:14:27 +0200

    nedokoncano

--- esp32/main.py
+++ esp32/main.py
@@ -7,20 +7,17 @@ def scan_rfid():
     print("Place your RFID tag near the reader")
 
     while True:
-        # Scan for cards
         (status, tag_type) = rdr.request(rdr.PICC_REQIDL)
         
         if status == rdr.MI_OK:
             print("Card detected!")
 
-            # Get the UID of the card
             (status, uid) = rdr.anticoll()
             
             if status == rdr.MI_OK:
                 print("UID:", uid)
-                time.sleep(2)  # Wait a bit before scanning again
+                time.sleep(2) 
         
-        time.sleep(1)  # Wait for a second before scanning again
+        time.sleep(1) 
 
-# Run the RFID scan
 scan_rfid()
