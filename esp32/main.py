f9de25bb0b3ec0cce276518878a56e6f7164e579
Kos1r <128988818+Kos1r@users.noreply.github.com>
2025-05-06 19:52:35 +0200

    Update main.py

--- esp32/main.py
+++ esp32/main.py
@@ -1,23 +1,39 @@
+from machine import Pin, SPI
 from mfrc522 import MFRC522
 import time
 
-def scan_rfid():
-    rdr = MFRC522()
+# definicije pinou
+sda_pin = 5  
+sck_pin = 18 
+mosi_pin = 23 
+miso_pin = 19 
+rst_pin = 4   
 
+# Initialize SPI communication
+spi = SPI(1, baudrate=1000000, polarity=0, phase=0, sck=Pin(sck_pin), mosi=Pin(mosi_pin), miso=Pin(miso_pin))
+rdr = MFRC522(spi=spi, gpioRst=rst_pin, gpioCs=sda_pin)
+
+def scan_rfid():
     print("Place your RFID tag near the reader")
 
     while True:
-        (status, tag_type) = rdr.request(rdr.PICC_REQIDL)
-        
-        if status == rdr.MI_OK:
+        # Request for a card
+        status, tag_type = rdr.request(0x26)
+
+        if status == rdr.OK:
             print("Card detected!")
 
-            (status, uid) = rdr.anticoll()
-            
-            if status == rdr.MI_OK:
-                print("UID:", uid)
-                time.sleep(2) 
-        
-        time.sleep(1) 
+            # Read the UID of the card
+            status, uid = rdr.anticoll()
 
+            if status == rdr.OK:
+                # konverta v lepso oblikso
+                uid_str = ''.join(['{:02X}'.format(x) for x in uid])
+                print("UID (Hex):", uid_str)
+                time.sleep(2)
+
+        time.sleep(1)
+
+#pozene funkcijo
 scan_rfid()
+
